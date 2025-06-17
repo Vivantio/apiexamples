5add62076151e14280e0875f9c44f93dbaf1d878
Graham Smith <graham.david.smith@gmail.com>
2025-06-17 08:29:25 +0100

    Update ticket DTO

--- VivantioApiInteractive/Ticket.cs
+++ VivantioApiInteractive/Ticket.cs
@@ -6,19 +6,61 @@ namespace VivantioApiInteractive
 
     public class TicketBaseDto
     {
-        public int Id { get; }
-        public string? DisplayId { get; }
-        public int? ClientId { get; set; }
-        public int? CallerId { get; set; }
-        public string? Description { get; set; }
-        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public int Id { get;}
+        public DateTime OpenDate { get; set; }
+        public DateTime CloseDate { get; set; }
+        public string? CallerName { get; set; }
+        public int TakenById { get; set; }
+        public int OwnerId { get; set; }
+        public int ThirdPartyId { get; set; }
+        public string? ThirdPartyRef { get; set; }
+        public DateTime ThirdPartyLogDate { get; set; }
+        public int PriorityId { get; set; }
         public StatusType? StatusType { get; set; }
-        public int? StatusId { get; set; }
-        public string? StatusName { get; set; }
-        public int? PriorityId { get; set; }
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+        public string? Solution { get; set; }
+        public int LocationId { get; set; }
+        public int LastActionId { get; set; }
+        public string? CallerPhone { get; set; }
+        public string? CallerEmail { get; set; }
+        public int TotalEffort { get; set; }
+        public int LastModifiedById { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public int GroupId { get; set; }
+        public int ClientId { get; set; }
+        public int StatusId { get; set; }
+        public int ImpactId { get; set; }
+        public int CallerId { get; set; }
         public string? PriorityName { get; set; }
-        public int? CategoryId { get; set; }
+        public string? TakenByName { get; set; }
+        public string? GroupName { get; set; }
+        public string? OwnerName { get; set; }
+        public string? ClientName { get; set; }
         public string? CategoryLineage { get; set; }
+        public string? StatusName { get; set; }
+        public int WorkflowId { get; set; }
+        public string? DisplayId { get; set; }
+        public bool Resolved { get; set; }
+        public bool IsDeferred { get; set; }
+        public int AssigneeType { get; set; }
+        public int CallOrigin { get; set; }
+        public int AssigneeClientId { get; set; }
+        public int AssigneeCallerId { get; set; }
+        public string? AssigneeExpression { get; set; }
+        public string? AssignedToEmail { get; set; }
+        public int IntelligentAssignmentGroup { get; set; }
+        public bool IntelligentAssignmentExcludeOutOfOffice { get; set; }
+        public string? AssignedToGroupName { get; set; }
+        public string? AssignedToIndividualName { get; set; }
+        public DateTime ResolvedDate { get; set; }
+        public string? AssignedToSummary { get; set; }
+        public string? CCAddressList { get; set; }
+        public string? DescriptionHtml { get; set; }
+        public string? SolutionHtml { get; set; }
+        public bool Deleted { get; set; }
+        public int NextStepId { get; set; }
+        public string? RecordTypeNameSingular { get; set; }
     }
 
     public class TicketInsertDto : TicketBaseDto
