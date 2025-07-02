fd39a380d8456448c45879db15524136822b6c52
Graham Smith <graham.smith@vivantio.com>
2025-07-02 06:44:31 +0100

    Typp

--- VivantioApiInteractive/Ticket.cs
+++ VivantioApiInteractive/Ticket.cs
@@ -124,7 +124,7 @@ namespace VivantioApiInteractive
 
             var ticket = new TicketInsertDto
             {
-                RecordTypeId = 11, // Incidents
+                RecordTypeId = 11, // Incidents t
                 ClientId = selectedClient.Id,
                 CallerId = 1,
                 Title = title,
